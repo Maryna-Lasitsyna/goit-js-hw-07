f062979ee2c15289c33c3db46f1b6302d805fcdb
Maryna-Lasitsyna <121902242+Maryna-Lasitsyna@users.noreply.github.com>
2023-05-18 01:11:59 +0300

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,55 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const 
+const galleryEl = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      (item) =>
+        `<li class="gallery__item">
+           <a class="gallery__link" href="${item.original}">
+           <img
+           class="gallery__image"
+           src="${item.preview}"  
+           alt="${item.description}"" 
+           />
+           </a>
+         </li>`
+    )
+    .join("");
+}
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryEl.innerHTML = addGalleryMarkup;
+
+galleryEl.addEventListener("click", onImageClick);
+
+function onImageClick(evt) {
+  blockStandartAction(evt);
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "bottom",
+  });
+
+  lightbox.show();
+
+  galleryEl.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function blockStandartAction(evt) {
+  evt.preventDefault();
+}
+
 console.log(galleryItems);
