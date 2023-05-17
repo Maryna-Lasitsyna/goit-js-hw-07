d40fc4161159b793e77d8ca7092a24489696d954
Maryna-Lasitsyna <121902242+Maryna-Lasitsyna@users.noreply.github.com>
2023-05-17 22:46:50 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+
+
+const galleryEl = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      (item) =>
+        `<li class="gallery-item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+          />
+        </a>
+      </li>`
+    )
+    .join("");
+}
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryEl.innerHTML = addGalleryMarkup;
+
+galleryEl.addEventListener("click", onImageClick);
+
+function onImageClick(evt) {
+  blockStandartAction(evt);
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  galleryEl.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function blockStandartAction(evt) {
+  evt.preventDefault();
+}
 
 console.log(galleryItems);
